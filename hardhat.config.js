b1823484c161de5bbd397e1449fb9ba69d172063
Leoni Mella <leoni.mella@gmail.com>
2026-01-15 14:04:37 -0300

    feat: add stable chain parameters

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,28 @@
-require("dotenv").config();
-require("solidity-coverage");
-require('hardhat-deploy');
-require('hardhat-deploy-ethers');
-require("@nomiclabs/hardhat-etherscan");
-require('./tasks');
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks")
 
 function getMnemonic(networkName) {
-  if (networkName) {
-    const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()]
-    if (mnemonic && mnemonic !== '') {
-      return mnemonic
+    if (networkName) {
+        const mnemonic = process.env["MNEMONIC_" + networkName.toUpperCase()]
+        if (mnemonic && mnemonic !== "") {
+            return mnemonic
+        }
     }
-  }
 
-  const mnemonic = process.env.MNEMONIC
-  if (!mnemonic || mnemonic === '') {
-    return 'test test test test test test test test test test test junk'
-  }
+    const mnemonic = process.env.MNEMONIC
+    if (!mnemonic || mnemonic === "") {
+        return "test test test test test test test test test test test junk"
+    }
 
-  return mnemonic
+    return mnemonic
 }
 
 function accounts(chainKey) {
-  return { mnemonic: getMnemonic(chainKey) }
+    return { mnemonic: getMnemonic(chainKey) }
 }
 
 // You need to export an object to set up your config
@@ -32,97 +32,101 @@ function accounts(chainKey) {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.17",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 1
-          }
-        }
-      }
-    ]
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,    // wallet address 0, of the mnemonic in .env
-    }
-  },
-
-  networks: {
-    ethereum: {
-      url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      chainId: 1,
-      accounts: accounts(),
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.17",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1,
+                    },
+                },
+            },
+        ],
     },
-    bsc: {
-      url: "https://bsc-dataseed1.binance.org",
-      chainId: 56,
-      accounts: accounts(),
-    },
-    avalanche: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      chainId: 43114,
-      accounts: accounts(),
-    },
-    polygon: {
-      url: "https://rpc-mainnet.maticvigil.com",
-      chainId: 137,
-      accounts: accounts(),
-    },
-    arbitrum: {
-      url: `https://arb1.arbitrum.io/rpc`,
-      chainId: 42161,
-      accounts: accounts(),
-    },
-    optimism: {
-      url: `https://mainnet.optimism.io`,
-      chainId: 10,
-      accounts: accounts(),
-    },
-    fantom: {
-      url: `https://rpcapi.fantom.network`,
-      chainId: 250,
-      accounts: accounts(),
+    namedAccounts: {
+        deployer: {
+            default: 0, // wallet address 0, of the mnemonic in .env
+        },
     },
 
-    goerli: {
-      url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      chainId: 5,
-      accounts: accounts(),
+    networks: {
+        ethereum: {
+            url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            chainId: 1,
+            accounts: accounts(),
+        },
+        bsc: {
+            url: "https://bsc-dataseed1.binance.org",
+            chainId: 56,
+            accounts: accounts(),
+        },
+        avalanche: {
+            url: "https://api.avax.network/ext/bc/C/rpc",
+            chainId: 43114,
+            accounts: accounts(),
+        },
+        polygon: {
+            url: "https://rpc-mainnet.maticvigil.com",
+            chainId: 137,
+            accounts: accounts(),
+        },
+        arbitrum: {
+            url: `https://arb1.arbitrum.io/rpc`,
+            chainId: 42161,
+            accounts: accounts(),
+        },
+        optimism: {
+            url: `https://mainnet.optimism.io`,
+            chainId: 10,
+            accounts: accounts(),
+        },
+        fantom: {
+            url: `https://rpcapi.fantom.network`,
+            chainId: 250,
+            accounts: accounts(),
+        },
+        stable: {
+            url: "https://rpc.stable.xyz",
+            chainId: 988,
+            accounts: accounts(),
+        },
+
+        goerli: {
+            url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            chainId: 5,
+            accounts: accounts(),
+        },
+        "bsc-testnet": {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+            chainId: 97,
+            accounts: accounts(),
+        },
+        fuji: {
+            url: `https://api.avax-test.network/ext/bc/C/rpc`,
+            chainId: 43113,
+            accounts: accounts(),
+        },
+        mumbai: {
+            url: "https://rpc-mumbai.maticvigil.com/",
+            chainId: 80001,
+            accounts: accounts(),
+        },
+        "arbitrum-goerli": {
+            url: `https://goerli-rollup.arbitrum.io/rpc/`,
+            chainId: 421613,
+            accounts: accounts(),
+        },
+        "optimism-goerli": {
+            url: `https://goerli.optimism.io/`,
+            chainId: 420,
+            accounts: accounts(),
+        },
+        "fantom-testnet": {
+            url: `https://rpc.testnet.fantom.network/`,
+            chainId: 4002,
+            accounts: accounts(),
+        },
     },
-    'bsc-testnet': {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-      chainId: 97,
-      accounts: accounts(),
-    },
-    fuji: {
-      url: `https://api.avax-test.network/ext/bc/C/rpc`,
-      chainId: 43113,
-      accounts: accounts(),
-    },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/",
-      chainId: 80001,
-      accounts: accounts(),
-    },
-    'arbitrum-goerli': {
-      url: `https://goerli-rollup.arbitrum.io/rpc/`,
-      chainId: 421613,
-      accounts: accounts(),
-    },
-    'optimism-goerli': {
-      url: `https://goerli.optimism.io/`,
-      chainId: 420,
-      accounts: accounts(),
-    },
-    'fantom-testnet': {
-      url: `https://rpc.testnet.fantom.network/`,
-      chainId: 4002,
-      accounts: accounts(),
-    }
-  }
-};
+}
